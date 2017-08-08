843349ec44a6846e8f167abf8780534e027cb7d3
aa.kornev <aa.kornev@tensor.ru>
2017-08-08 09:31:33 +0300

    net

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+'use strict';
 var express = require('express'),
    app = express(),
    http = require('http'),
@@ -16,9 +17,30 @@ mongoClient.connect(config.get('mongoose'), function(err, db) {
    if (err) {
       return intel.error(err);
    }
-   require('./routes')(app, db);
+   // require('./routes')(app, db);
 });
-var server = http.createServer(app);
+var server = net.createServer(function(socket) {
+   console.log("client connected!!!!");
+
+   var data = '';
+   socket.on('data', function(d) {
+      console.log(d.toString('utf8').indexOf('\n'));
+      data += d.toString('utf8');
+      var p = data.indexOf('\n');
+      if (~p) {
+         var cmd = data.substr(0, p);
+         data = data.slice(p + 1);
+         onCommand(cmd.trim(), socket);
+         data='';
+      }
+      socket.write(d);
+   });
+
+   socket.on('end', function() {
+      console.log('client disconnected');
+   });
+
+});
 
 server.on('connection', function(err, socket) {
    console.log('hi');
@@ -27,3 +49,17 @@ server.on('connection', function(err, socket) {
 server.listen(config.get('port'), function() {
    intel.info('connetion was start on' + config.get('port'));
 });
+
+function onCommand(cmd, socket) {
+   switch (cmd) {
+      case 'open':
+         socket.write('opened\n');
+         break;
+      case 'add':
+         socket.write('added\n');
+         break;
+      case 'process':
+         socket.write('processed\n');
+         break;
+   }
+}
